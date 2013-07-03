ae308697f15290de79b647b0965bc42312d446fc
James Brooks <jbrooksuk@me.com>
2013-07-03 15:37:34 +0100

    Added example.js to tests
    Tests are not available yet

---
+++ tests/example.js
@@ -0,0 +1,7 @@
+SoundRain = require('../');
+var Song = new SoundRain("https://soundcloud.com/jbrooksuk/twisted-beat-sample-at", '.');
+Song.on('error', function(err) {
+    if(err) throw err;
+}).on('done', function(file) {
+    console.log(file);
+});
